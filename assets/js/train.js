97605180b7746ee62a65d3fa5afa4742185f96e0
Yadira Soto <lunar13heart@hotmail.com>
2019-06-13 18:59:29 -0400

    added firebase links to js & html

--- assets/js/train.js
+++ assets/js/train.js
@@ -1,1 +1,15 @@
-// my js will go here
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#config-web-app -->
+
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyCPGm4_0j3pWqbT8uISmIJO4jESE9UL0IQ",
+    authDomain: "train-scheduler-c28ae.firebaseapp.com",
+    databaseURL: "https://train-scheduler-c28ae.firebaseio.com",
+    projectId: "train-scheduler-c28ae",
+    storageBucket: "train-scheduler-c28ae.appspot.com",
+    messagingSenderId: "841403943147",
+    appId: "1:841403943147:web:e8194e720ff3103d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
