6680d17750437066289476e67123b675a29466e4
Yadira Soto <lunar13heart@hotmail.com>
2019-06-13 21:37:08 -0400

    js & html edits

--- assets/js/train.js
+++ assets/js/train.js
@@ -1,6 +1,3 @@
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#config-web-app -->
-
 // Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyCPGm4_0j3pWqbT8uISmIJO4jESE9UL0IQ",
@@ -11,63 +8,80 @@ var firebaseConfig = {
     messagingSenderId: "841403943147",
     appId: "1:841403943147:web:e8194e720ff3103d"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-$(document).ready(function () {
-    console.log("ready!");
+var databse = firebase.database();
 
 
+var trainNames;
+var trainDestinations;
+var trainFrequencies;
+var firstTrain;
+var trainsNextArrival;
+var trainsMinutesAway;
+
+$("#add-train").on("click", function (event) {
+
+    event.preventDefault(event);
+
+    trainNames = $("#name-input").val().trim().
+    trainDestinations = $("#destination-input").val().trim().
+    trainFrequencies = $("#frequency-input").val().trim().
+    firstTrain = $("#time-input").val().trim().
+
+    console.log(trainNames);
+    console.log(trainDestinations);
+    console.log(trainFrequencies);
+    console.log(firstTrain);
+
+    database.ref().push({
+        dataTrainNames: trainNames,
+        dataTrainDestinations: trainDestinations,
+        dataTrainFrequencies: trainFrequencies,
+        dataFirstTrain: firstTrain,
+        dataNextArrival= trainsNextArrival,
+        dataMinutesArrival= trainsMinutesAway
+    });
 
 
+    alert("Train Added");
+
+    $("#name-input").text("");
+    $("#destination-input").text("");
+    $("#frequency-input").text("");
+    $("#time-input").text("");
+
+    database.ref().on("child_added", function(databaseData) {
+
+        console.log(databaseData).val()
+
+        var tnName = databaseData.val().dataTrainNames;
+        var tnDestinations = databaseData.val().dataTrainDestinations;
+        var tnFrequencies = databaseData.val().dataTrainFrequencies;
+        var tnFirstTrain = databaseData.val().dataFirstTrain;
+        var tnNextArrival = databaseData.val().dataFirstTrain;
+        var tnMinutesAway = databaseData.val().dataFirstTrain;
 
 
+        // new arrival and minutes away section
 
 
+        var tr = $("<tr");
+        var tdNames = $("<td>").text(tnName);
+        var tdDestinations = $("<td>").text(tnDestinations);
+        var tdFrequencies = $("<td>").text(tnFrequencies);
+        var tdFirstTrain = $("<td>").text(tnFirstTrain);
+        var tdNextArrival = $("<td>").text(tnNextArrival);
+        var tdMinutesAway = $("<td>").text(tnMinutesAway);
+
+        tr.append(tdNames, tdDestinations, tdFrequencies, tdFirstTrain, tdNextArrival, tdMinutesAway);
+
+        td.append(tdNames, tdDestinations, tdFrequencies, tdFirstTrain, tdNextArrival, tdMinutesAway);
+
+        $("#card-body").append.tr
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    });
 });
-
-
-
-
